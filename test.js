ee6c17e9385470d50405a0d3b2cf48008f79b70c
brightchul <abhbtbb1@gmail.com>
2023-02-17 17:47:42 +0900

    chore: add next.js setting

---
+++ test.js
@@ -0,0 +1,32 @@
+const initialData = [
+  {
+    id: 0,
+    title: "터미널",
+    comment: "슬픈 영화인 줄 알았는데 코미디!",
+    score: 4,
+  },
+  {
+    id: 1,
+    title: "사랑에 대한 모든 것",
+    comment: "스티븐 호킹의 감동 실화ᅲᅲ",
+    score: 5,
+  },
+  {
+    id: 2,
+    title: "터미네이터: 다크 페이트",
+    comment: "내 취향은 아니었던 걸로...",
+    score: 2,
+  },
+  {
+    id: 3,
+    title: "날씨의 아이",
+    comment: "그럭저럭 볼만했던 영화 :)",
+    score: 3,
+  },
+  {
+    id: 4,
+    title: "미나리",
+    comment: "윤여정 선생님의 연기가 너무 멋졌다.",
+    score: 5,
+  },
+];
