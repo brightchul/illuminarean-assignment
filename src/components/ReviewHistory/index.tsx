7d428ee1c178b3373fe0b60c0f99a62073f0e2af
brightchul <abhbtbb1@gmail.com>
2023-02-19 16:41:43 +0900

    feat: add soted movies

--- src/components/ReviewHistory/index.tsx
+++ src/components/ReviewHistory/index.tsx
@@ -1,14 +1,19 @@
+import { useMemo } from "react";
+
 import { useMovieStore } from "@/stores";
+import { getSortMovies } from "@/utils";
+
 import ReviewCard from "../ReviewCard";
 
 export default function ReviewHistory() {
   const movies = useMovieStore((state) => state.movies);
+  const sortedMovies = useMemo(() => getSortMovies(movies), [movies]);
 
   return (
     <section className="flex flex-col gap-4 bg-[#f5f5f5] p-4">
       <h2 className="text-lg font-bold text-[#194e84]">리뷰 내역</h2>
       <div className="flex flex-col gap-4">
-        {movies.map((movie, idx) => (
+        {sortedMovies.map((movie, idx) => (
           <ReviewCard key={idx} {...movie} />
         ))}
       </div>
